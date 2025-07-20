35dd9825836bb54a724d588293ef62868bbbb3c9
Roman Ishtokin <ishtokin.roman@gmail.com>
2025-07-20 16:23:59 +0900

    sprint-7: Добавить реализацию менеджера с сохранением в файл

---
+++ src/Exceptions/ManagerSaveException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class ManagerSaveException extends RuntimeException {
+    public ManagerSaveException(String massage, Throwable cause) {
+        super(massage, cause);
+    }
+}
--- src/manager/InMemoryTaskManager.java
+++ src/manager/InMemoryTaskManager.java
@@ -11,9 +11,9 @@ import java.util.List;
 
 public class InMemoryTaskManager implements TaskManager {
     private int idCounter = 1;
-    private final HashMap<Integer, Task> tasks = new HashMap<>();
-    private final HashMap<Integer, Subtask> subtasks = new HashMap<>();
-    private final HashMap<Integer, Epic> epics = new HashMap<>();
+    protected final HashMap<Integer, Task> tasks = new HashMap<>();
+    protected final HashMap<Integer, Subtask> subtasks = new HashMap<>();
+    protected final HashMap<Integer, Epic> epics = new HashMap<>();
     private final HistoryManager historyManager = Managers.getDefaultHistory();
 
     @Override
